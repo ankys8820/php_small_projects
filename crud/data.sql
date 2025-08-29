a186ed928952a4225c797d1f60c292e57e2544aa
ankys8820 <anky8820@outlook.com>
2025-08-29 14:30:36 +0530

    crud : Creating the data and showing data on frontend.

--- crud/data.sql
+++ crud/data.sql
@@ -9,7 +9,7 @@ CREATE TABLE users(
     firstname VARCHAR(255) NOT NULL,
     lastname VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
-    passowrd VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
     gender VARCHAR(25) NOT NULL
 );
 
@@ -19,9 +19,19 @@ DESC users;
 
 -- for deleting one column from the table 
 
-ALTER TABLE users DROP COLUMN gender;
+ALTER TABLE users DROP COLUMN lastname;
+
+-- change the column name 
+ALTER TABLE users
+CHANGE firstname name VARCHAR(255);
+
+--
+ALTER TABLE users
+CHANGE passowrd password VARCHAR(255); 
 
 
+ALTER TABLE users ADD COLUMN mobile VARCHAR(20) NOT NULL AFTER email;
+
+SELECT * FROM users;
 
 -- Creation of new data into it
-
---
+++ crud/user.php
@@ -0,0 +1,78 @@
+<?php
+include 'connect.php';
+
+if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $mobile = $_POST['mobile'];
+    $password = $_POST['password'];
+
+    $sql = "INSERT INTO `users` (`name`,`email`,`mobile`, `password`) VALUE ('$name','$email','$mobile','$password')";
+
+    $result = mysqli_query($con, $sql);
+
+    if ($result) {
+        echo "<script>
+        alert('Record added successfully!');
+      </script>";
+    } else {
+        echo "<script>
+        alert('Some error occured!');
+      </script>";
+        die(mysqli_error($con));
+    }
+}
+
+
+?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Create</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container mb-3">
+        <h3 class="m-4">Add your Users Details here !</h3>
+        <form method="post">
+            <div class="mb-3">
+                <input type="text" class="form-control"
+                    name="name"
+                    placeholder="Enter your name" autocomplete="off">
+            </div>
+            <div class="mb-3">
+                <input type="email" class="form-control"
+                    name="email"
+                    placeholder="Enter your email" autocomplete="off">
+            </div>
+            <div class="mb-3">
+                <input type="text" class="form-control"
+                    name="mobile"
+                    placeholder="Enter your mobile number" autocomplete="off">
+            </div>
+
+            <div class="mb-3">
+                <input type="password" class="form-control"
+                    name="password"
+                    placeholder="Enter your password" autocomplete="off">
+            </div>
+
+            <button type="submit" class="btn btn-dark" name="submit">Submit</button>
+
+        </form>
+        <p class="mt-2"><a class="cursor-pointer" href="index.php">Click here</a> see the list of data !</p>
+
+
+    </div>
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+</body>
+
+</html>
