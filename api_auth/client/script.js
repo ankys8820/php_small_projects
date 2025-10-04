2a7dc04b80be52e8741a3480eba87bfd14966bba
ankys8820 <ankys8820@outlook.com>
2025-10-04 13:06:18 +0530

    features : basic landing page is create for the project

---
+++ api_auth/client/script.js
@@ -0,0 +1,10 @@
+
+const signup = document.querySelector('.signup');
+
+const login = document.querySelector('.login');
+
+const update = document.querySelector('.update');
+
+const logout = document.querySelector('.logout');
+
+const dlt = document.querySelector('.delete');
--- api_auth/server/Model.php
+++ api_auth/server/Model.php
@@ -1,8 +1,35 @@
 <?php
 include_once './db.php';
 
-function subscribe($conn){}
+// for subscribe
+if($_SERVER['REQUEST_METHOD'] == "POST" && $_POST['crud_req']=='subscribe')
+    subscribe($conn);
+
+// for logout
+if($_SERVER['REQUEST_METHOD'] == "GET")
+    logout($conn);
+
+
+// for update
+if($_SERVER['REQUEST_METHOD'] == "PATCH")
+    update($conn);
+
+// UNSUBSCRIBE
+
+if($_SERVER['REQUEST_METHOD'] == "DELETE")
+    unSubscribe($conn);
+
+// for login
+if($_SERVER['REQUEST_METHOD'] == "POST" && $_POST['crud_req']=='login')
+    login($conn);
+
+
+
+function subscribe($conn){
+    
+}
 function login($conn){}
 function logout($conn){}
 function update($conn){}
 function unSubscribe($conn){}
+
